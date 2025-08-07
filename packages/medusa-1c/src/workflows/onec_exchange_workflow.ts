0ba716cad71c988c8eff7c80477ebb6b8a55b7c1
PoSayDone <poseaydone@ya.ru>
2025-08-07 13:10:47 +0500

    feat: auth from medusa

--- packages/medusa-1c/src/workflows/onec_exchange_workflow.ts
+++ packages/medusa-1c/src/workflows/onec_exchange_workflow.ts
@@ -1,199 +1,179 @@
 import {
-	createProductsWorkflow,
-	updateProductsWorkflow,
-	useQueryGraphStep,
+  createProductsWorkflow,
+  updateProductsWorkflow,
+  useQueryGraphStep,
 } from "@medusajs/medusa/core-flows";
 import {
-	createWorkflow,
-	transform,
-	WorkflowResponse,
+  createWorkflow,
+  transform,
+  WorkflowResponse,
 } from "@medusajs/framework/workflows-sdk";
 import {
-	CreateProductWorkflowInputDTO,
-	ProductCategoryDTO,
-	UpdateProductWorkflowInputDTO,
+  CreateProductWorkflowInputDTO,
+  ProductCategoryDTO,
+  UpdateProductWorkflowInputDTO,
 } from "@medusajs/framework/types";
-import { ProductDTO, ShippingProfileDTO } from "@medusajs/types";
+import {
+  CreateProductVariantWorkflowInputDTO,
+  ProductDTO,
+  ShippingProfileDTO,
+} from "@medusajs/types";
 import _ from "lodash";
 import slugify from "sluga";
-import { parseFilesStep } from "../steps/parse-files";
+import { flattenClassifierGroups } from "../utils/product-utils";
 import {
-	flattenClassifierGroups,
-	parseDictionaryOptions,
-	parseProductOptions,
-} from "../utils/product-utils";
-import { OnecData, OnecExchangeWorkflowInput } from "../types";
+  ImportOutput,
+  OnecExchangeWorkflowInput,
+  OffersOutput,
+} from "../types";
 import { processCategoriesRecursivelyStep } from "../steps/process-categories";
+import { parseImportFilesStep } from "../steps/parse-import-files";
+import { parseOffersFilesStep } from "../steps/parse-offers-files";
 
 export const onecExchangeWorkflow = createWorkflow(
-	"sync-from-erp",
-	(input: OnecExchangeWorkflowInput) => {
-		const onecData = parseFilesStep(input);
+  "sync-from-erp",
+  (input: OnecExchangeWorkflowInput) => {
+    const importData = parseImportFilesStep(input.import);
+    const offersData = parseOffersFilesStep(input.offers);
 
-		const { data: stores } = useQueryGraphStep({
-			entity: "store",
-			fields: ["default_sales_channel_id"],
-		}).config({ name: "stores" });
+    const { data: stores } = useQueryGraphStep({
+      entity: "store",
+      fields: ["default_sales_channel_id", "default_currency_code"],
+    }).config({ name: "stores" });
 
-		const { data: shippingProfiles } = useQueryGraphStep({
-			entity: "shipping_profile",
-			fields: ["id"],
-			pagination: { skip: 0, take: 1 },
-		}).config({ name: "shipping-profile" });
+    const { data: shippingProfiles } = useQueryGraphStep({
+      entity: "shipping_profile",
+      fields: ["id"],
+      pagination: { skip: 0, take: 1 },
+    }).config({ name: "shipping-profile" });
 
-		const externalIdsFilters = transform({ onecData }, (data) =>
-			data.onecData.products.map((product) => `${product.id}`),
-		);
+    const externalIdsFilters = transform({ onecData: importData }, (data) =>
+      data.onecData.products.map((product) => `${product.id}`)
+    );
 
-		const { data: existingProducts } = useQueryGraphStep({
-			entity: "product",
-			fields: ["id", "external_id", "variants.*"],
-			filters: { external_id: externalIdsFilters },
-		}).config({ name: "existing-products" });
+    const { data: existingProducts } = useQueryGraphStep({
+      entity: "product",
+      fields: ["id", "external_id", "variants.*"],
+      filters: { external_id: externalIdsFilters },
+    }).config({ name: "existing-products" });
 
-		const externalCatgoriesIdsFilters = transform({ onecData }, (data) =>
-			flattenClassifierGroups(data.onecData.classifierGroups).map(
-				(cg) => cg.id,
-			),
-		);
+    const externalCatgoriesIdsFilters = transform(
+      { onecData: importData },
+      (data) =>
+        flattenClassifierGroups(data.onecData.classifierGroups).map(
+          (cg) => cg.id
+        )
+    );
 
-		const categoryHandlesFilters = transform({ onecData }, (data) => [
-			...new Set(
-				flattenClassifierGroups(data.onecData.classifierGroups).map(
-					(cg) => slugify(cg.name),
-				),
-			),
-		]);
+    const categoryHandlesFilters = transform(
+      { onecData: importData },
+      (data) => [
+        ...new Set(
+          flattenClassifierGroups(data.onecData.classifierGroups).map((cg) =>
+            slugify(cg.name)
+          )
+        ),
+      ]
+    );
 
-		const { data: existingCategories } = useQueryGraphStep({
-			entity: "product_category",
-			fields: ["id", "handle", "metadata"],
-			filters: {
-				$or: [
-					{ metadata: { onec_id: externalCatgoriesIdsFilters } },
-					{ handle: categoryHandlesFilters },
-				],
-			},
-		}).config({ name: "existing-categories" });
+    const { data: existingCategories } = useQueryGraphStep({
+      entity: "product_category",
+      fields: ["id", "handle", "metadata"],
+      filters: {
+        $or: [
+          { metadata: { onec_id: externalCatgoriesIdsFilters } },
+          { handle: categoryHandlesFilters },
+        ],
+      },
+    }).config({ name: "existing-categories" });
 
-		const processedCategories = processCategoriesRecursivelyStep({
-			classifierGroups: onecData.classifierGroups,
-			existingCategories,
-		});
+    const processedCategories = processCategoriesRecursivelyStep({
+      classifierGroups: importData.classifierGroups,
+      existingCategories,
+    });
 
-		const { productsToCreate, productsToUpdate } = transform(
-			{
-				processedCategories,
-				existingProducts,
-				shippingProfiles,
-				stores,
-				onecData,
-			},
-			(data: {
-				processedCategories: ProductCategoryDTO[];
-				existingProducts: ProductDTO[];
-				shippingProfiles: ShippingProfileDTO[];
-				stores: { default_sales_channel_id: string }[];
-				onecData: OnecData;
-			}) => {
-				const productsToCreate: CreateProductWorkflowInputDTO[] = [];
-				const productsToUpdate: UpdateProductWorkflowInputDTO[] = [];
+    const { productsToCreate, productsToUpdate } = transform(
+      {
+        processedCategories,
+        existingProducts,
+        shippingProfiles,
+        stores,
+        importData,
+        offersData,
+      },
+      (data: {
+        processedCategories: ProductCategoryDTO[];
+        existingProducts: ProductDTO[];
+        shippingProfiles: ShippingProfileDTO[];
+        stores: {
+          default_sales_channel_id: string;
+          default_currency_code: string;
+        }[];
+        importData: ImportOutput;
+        offersData: OffersOutput;
+      }) => {
+        const productsToCreate: CreateProductWorkflowInputDTO[] = [];
+        const productsToUpdate: UpdateProductWorkflowInputDTO[] = [];
 
-				const defaultOptions = [
-					{ title: "Default", values: ["Default"] },
-				];
-				const parsedOptions = parseDictionaryOptions(
-					data.onecData.properties,
-					data.onecData.settings?.attributes,
-				);
+        const defaultOptions = [
+          {
+            title: "Default option",
+            values: ["Default option value"],
+          },
+        ];
 
-				data.onecData.products.forEach((onecProduct) => {
-					const [defaultAttributes, variantOptions, metadata] =
-						parseProductOptions(
-							onecProduct,
-							data.onecData.properties,
-							data.onecData.settings?.attributes,
-						);
+        const defaultSalesChannelId =
+          data.stores[0]?.default_sales_channel_id || "";
+        const defaultCurrencyCode =
+          data.stores[0]?.default_currency_code || "rub";
 
-					const category_ids = data.processedCategories
-						.filter(
-							(cat) =>
-								cat.metadata?.onec_id === onecProduct.groupId,
-						)
-						.map((cat) => cat.id);
+        data.importData.products.forEach((onecProduct) => {
+          const variants: CreateProductVariantWorkflowInputDTO[] = [];
 
-					const productPayload = {
-						title: onecProduct.name,
-						category_ids,
-						description: onecProduct.description,
-						handle: slugify(onecProduct.name),
-						external_id: onecProduct.id,
-						variants: [],
-						metadata,
-						options:
-							parsedOptions.length > 0
-								? parsedOptions
-								: defaultOptions,
-						...defaultAttributes,
-					};
+          const category_ids = data.processedCategories
+            .filter((cat) => cat.metadata?.onec_id === onecProduct.groupId)
+            .map((cat) => cat.id);
 
-					const existingProduct = data.existingProducts.find(
-						(p) => p.external_id === productPayload.external_id,
-					);
+          const productPayload:
+            | CreateProductWorkflowInputDTO
+            | UpdateProductWorkflowInputDTO = {
+            title: onecProduct.name,
+            category_ids,
+            description: onecProduct.description,
+            handle: slugify(onecProduct.name),
+            external_id: onecProduct.id,
+            options: defaultOptions,
+            variants: variants,
+            sales_channels: [{ id: defaultSalesChannelId }],
+          };
 
-					if (existingProduct) {
-						const existingVariant = existingProduct.variants?.[0];
-						const updatePayload = {
-							...productPayload,
-							id: existingProduct.id,
-							variants: existingVariant
-								? [
-										{
-											id: existingVariant.id,
-											title: "Default variant",
-											barcode: onecProduct.barcode,
-											sku: onecProduct.article,
-											options: _.isEmpty(variantOptions)
-												? { Default: "Default" }
-												: variantOptions,
-										},
-									]
-								: [],
-						};
-						productsToUpdate.push(
-							updatePayload as UpdateProductWorkflowInputDTO,
-						);
-					} else {
-						const createPayload = {
-							...productPayload,
-							variants: [
-								{
-									title: "Default variant",
-									barcode: onecProduct.barcode,
-									sku: onecProduct.article,
-									options: _.isEmpty(variantOptions)
-										? { Default: "Default" }
-										: variantOptions,
-								},
-							],
-						};
-						productsToCreate.push(
-							createPayload as CreateProductWorkflowInputDTO,
-						);
-					}
-				});
+          const existingProduct = data.existingProducts.find(
+            (p) => p.external_id === productPayload.external_id
+          );
 
-				return { productsToCreate, productsToUpdate };
-			},
-		);
+          if (existingProduct) {
+            productsToUpdate.push({
+              ...productPayload,
+              id: existingProduct.id,
+            } as UpdateProductWorkflowInputDTO);
+          } else {
+            productsToCreate.push(
+              productPayload as CreateProductWorkflowInputDTO
+            );
+          }
+        });
 
-		createProductsWorkflow.runAsStep({
-			input: { products: productsToCreate },
-		});
-		updateProductsWorkflow.runAsStep({
-			input: { products: productsToUpdate },
-		});
+        return { productsToCreate, productsToUpdate };
+      }
+    );
 
-		return new WorkflowResponse({ input });
-	},
+    createProductsWorkflow.runAsStep({
+      input: { products: productsToCreate },
+    });
+    updateProductsWorkflow.runAsStep({
+      input: { products: productsToUpdate },
+    });
+
+    return new WorkflowResponse({ input });
+  }
 );
