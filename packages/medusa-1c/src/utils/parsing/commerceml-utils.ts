97844c77deeb5f842821657c528e35fab598c925
gorgohead <head@gorgojs.com>
2025-08-15 13:28:09 +0300

    style(medusa-1c): improve mapping syntax

--- packages/medusa-1c/src/utils/parsing/commerceml-utils.ts
+++ packages/medusa-1c/src/utils/parsing/commerceml-utils.ts
@@ -6,46 +6,46 @@ import { Counterparty } from "commerceml-parser-core";
  */
 export function parseCounterpartyXmlData(xmlData: any): Counterparty {
   const counterparty: Counterparty = {
-    id: xmlData.Ид,
-    name: xmlData.Наименование,
+    id: xmlData["Ид"],
+    name: xmlData["Наименование"],
   };
 
   // Detect company info or person info
-  if (xmlData.ОфициальноеНаименование) {
-    if (xmlData.ОфициальноеНаименование) {
-      counterparty.officialName = xmlData.ОфициальноеНаименование;
+  if (xmlData["ОфициальноеНаименование"]) {
+    if (xmlData["ОфициальноеНаименование"]) {
+      counterparty.officialName = xmlData["ОфициальноеНаименование"];
     }
 
-    if (xmlData.ИНН) {
-      counterparty.inn = xmlData.ИНН;
+    if (xmlData["ИНН"]) {
+      counterparty.inn = xmlData["ИНН"];
     }
 
-    if (xmlData.КПП) {
-      counterparty.kpp = xmlData.КПП;
+    if (xmlData["КПП"]) {
+      counterparty.kpp = xmlData["КПП"];
     }
 
-    if (xmlData.ОКПО) {
-      counterparty.okpo = xmlData.ОКПО;
+    if (xmlData["ОКПО"]) {
+      counterparty.okpo = xmlData["ОКПО"];
     }
   } else {
-    if (xmlData.ПолноеНаименование) {
-      counterparty.fullName = xmlData.ПолноеНаименование;
+    if (xmlData["ПолноеНаименование"]) {
+      counterparty.fullName = xmlData["ПолноеНаименование"];
     }
 
-    if (xmlData.Обращение) {
-      counterparty.title = xmlData.Обращение;
+    if (xmlData["Обращение"]) {
+      counterparty.title = xmlData["Обращение"];
     }
 
-    if (xmlData.Фамилия) {
-      counterparty.lastName = xmlData.Фамилия;
+    if (xmlData["Фамилия"]) {
+      counterparty.lastName = xmlData["Фамилия"];
     }
 
-    if (xmlData.Имя) {
-      counterparty.firstName = xmlData.Имя;
+    if (xmlData["Имя"]) {
+      counterparty.firstName = xmlData["Имя"];
     }
 
-    if (xmlData.Отчество) {
-      counterparty.middleName = xmlData.Отчество;
+    if (xmlData["Отчество"]) {
+      counterparty.middleName = xmlData["Отчество"];
     }
   }
 
