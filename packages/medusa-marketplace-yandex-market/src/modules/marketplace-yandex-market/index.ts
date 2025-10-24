8891445cd90254c6b89c589acfc641540e996f1a
Ismailov Maxim <guarus19@gmail.com>
2025-10-24 11:05:12 +0300

    chore: remove campaigns feature

--- packages/medusa-marketplace-yandex-market/src/modules/marketplace-yandex-market/index.ts
+++
@@ -1,8 +0,0 @@
-import MarketplaceYandexMarketModuleService from "./service"
-import { Module } from "@medusajs/framework/utils"
-
-export const MARKETPLACE_YANDEX_MARKET_MODULE = "marketplace-yandex-market"
-
-export default Module(MARKETPLACE_YANDEX_MARKET_MODULE, {
-  service: MarketplaceYandexMarketModuleService,
-})
