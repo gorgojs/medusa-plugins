25f6a5ae6ad6177ff1b913ec8e91f116d1faa9db
Ismailov Maxim <guarus19@gmail.com>
2025-11-10 16:48:59 +0300

    feat(medusa-marketplace-yandex-market): add step for mapping neccessary params

---
+++ packages/medusa-marketplace-yandex-market/src/workflows/product/steps/map-products-to-ym-format-step.ts
@@ -0,0 +1,71 @@
+import {
+    createStep,
+    StepResponse
+} from "@medusajs/workflows-sdk"
+import {
+    OfferWeightDimensionsDTO,
+
+} from "../../../lib/yandex-market-client/api"
+
+
+type itemsYm = {
+    offerId: string,
+    name: string,
+    description: string,
+    dimensions: OfferWeightDimensionsDTO,
+    vendor: string,
+    marketCategoryId: number,
+    pictures: Array<string>
+}
+
+
+function mapProductsToYmFormat(products) {
+    // TODO Add flexibile categoryId:
+    const marketCategoryId = Number(91491)
+    const vendor = "Medusa"
+    const productsList: any[] = Array.isArray(products) ? (products as any[]) : []
+    const ymItems: itemsYm[] = []
+
+    for (const product of productsList) {
+        const pictures: string[] = product.thumbnail
+            ? [product.thumbnail]
+            : []
+        if (!pictures.length) continue
+
+
+        const offerId = product.handle
+        const name = product.title
+        const description = product.description
+        const dimensions: OfferWeightDimensionsDTO = {
+            length: Number((product.length / 10).toFixed(2)),
+            width: Number((product.width / 10).toFixed(2)),
+            height: Number((product.height / 10).toFixed(2)),
+            weight: Number((product.weight / 100).toFixed(2))
+        }
+
+        ymItems.push({
+            offerId: offerId,
+            name: name,
+            description: description,
+            dimensions: dimensions,
+            vendor: vendor,
+            marketCategoryId: marketCategoryId,
+            pictures: pictures
+        })
+    }
+    return ymItems
+}
+
+
+export const mapProductsToYmFormatStep = createStep<unknown[], itemsYm[], void>(
+    "map-products-to-ym-format-step",
+    async (products) => {
+        const items = mapProductsToYmFormat(products)
+        return new StepResponse(items)
+    }
+)
+
+
+
+
+
