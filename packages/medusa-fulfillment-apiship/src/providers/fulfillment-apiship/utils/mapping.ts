1b62e17aaa5c9b9c896d82da0d5afeb974e70931
ttokyose <parsifal.cs@gmail.com>
2025-11-12 15:57:44 +0300

    fix(medusa-fulfillment-apiship): finish mapping the order creation Fixes: #57

--- packages/medusa-fulfillment-apiship/src/providers/fulfillment-apiship/utils/mapping.ts
+++ packages/medusa-fulfillment-apiship/src/providers/fulfillment-apiship/utils/mapping.ts
@@ -2,37 +2,44 @@ import {
   FulfillmentDTO,
   FulfillmentItemDTO,
   FulfillmentOrderDTO,
+  StockLocationDTO
 } from "@medusajs/framework/types"
 import {
   type OrderRequest,
-  type OrderReturnRequest,
 } from "../../../apiship-client"
+
+const ITEM_LENGTH = 10
+const ITEM_WIDTH = 10
+const ITEM_HEIGHT = 10
+const ITEM_WEIGHT = 20
 
 export function mapToApishipOrderRequest(
   data: Record<string, unknown>,
   items: Partial<Omit<FulfillmentItemDTO, "fulfillment">>[],
   order: Partial<FulfillmentOrderDTO>,
   fulfillment: Partial<Omit<FulfillmentDTO, "provider_id" | "data" | "items">>,
+  stockLocation: StockLocationDTO,
   providerKey: string,
   providerConnectId: string,
   tariffId: number,
+  isCod: boolean
 ): OrderRequest {
-  const fallbackItemWeight = 200
-  const fallbackItemSide = 20
-  const placeDimensions = { length: 40, width: 20, height: 20 }
+  const stolstockLocationAddress = stockLocation.address!
 
-  // TODO: parse real sender info
   const sender = {
-    countryCode: "RU",
-    city: "Москва",
-    addressString: "г Москва, ул Машкова, д 21",
+    countryCode: stolstockLocationAddress.country_code.toUpperCase(),
+    city: stolstockLocationAddress.city!,
+    region: stolstockLocationAddress.province!,
+    addressString: `${stolstockLocationAddress.city}, ${stolstockLocationAddress.address_1}, ${stolstockLocationAddress.address_2}`,
+    // TODO: parse real sender contact name (mb in provider option?)
     contactName: "Отправитель",
-    phone: "79990000000",
+    phone: stolstockLocationAddress.phone!,
+    companyName: stolstockLocationAddress.company!,
   }
 
   const shippingAddress = order.shipping_address!
   const recipient = {
-    countryCode: (shippingAddress.country_code || "RU").toUpperCase(),
+    countryCode: (shippingAddress.country_code!).toUpperCase(),
     postIndex: order.shipping_address?.postal_code,
     region: shippingAddress.province,
     city: shippingAddress.city,
@@ -43,23 +50,9 @@ export function mapToApishipOrderRequest(
     ].filter(Boolean).join(", "),
     contactName: [shippingAddress.first_name, shippingAddress.last_name].join(" ") as string,
     phone: shippingAddress.phone!,
-    ...(shippingAddress.company ? {companyName: shippingAddress.company} : {} ),
-    ...((order as any).customer.email ? {email: (order as any).customer.email} : {} ),
+    ...(shippingAddress.company ? { companyName: shippingAddress.company } : {}),
+    ...((order as any).customer.email ? { email: (order as any).customer.email } : {}),
   }
-
-  // TODO: when no weight on items
-  const totalWeight = order.items!.reduce((sum, item) => {
-    const quantity = item.quantity
-    const weight = (item as any).variant.weight || fallbackItemWeight
-    return sum + weight * quantity
-  }, 0)
-
-  const assessedCost =
-    order.items!.reduce((sum, item) => {
-      const unitPrice = item.unit_price
-      const quantity = item.quantity
-      return sum + unitPrice * quantity
-    }, 0) || Number(order.total)
 
   const placeItems = order.items!.map((item, idx) => {
     const quantity = item.quantity
@@ -69,30 +62,73 @@ export function mapToApishipOrderRequest(
     const width = (item as any).variant.width
     const articul = (item as any).variant.sku
     const description = `${item.title} ${item.subtitle}`
-    const cost = item.unit_price
-
+    const cost = isCod ? item.unit_price : 0
     return {
-      //height: height || fallbackItemSide,
-      //length: length || fallbackItemSide,
-      //width: width || fallbackItemSide,
-      weight: weight || fallbackItemWeight,
+      height: height || ITEM_HEIGHT,
+      length: length || ITEM_LENGTH,
+      width: width || ITEM_WIDTH,
+      weight: weight || ITEM_WEIGHT,
       articul: articul,
       description: description,
       quantity: quantity,
       assessedCost: cost,
       cost: cost,
       costVat: -1,
-      ...((width && height && length) ? {width, height, length} : {}),
-      ...((item as any).variant.barcode ? {barcode: (item as any).variant.barcode} : {}),
+      ...((item as any).variant.barcode ? { barcode: (item as any).variant.barcode } : {}),
     }
   })
 
+  const totalWeight = placeItems.reduce((sum, item) => {
+    const quantity = item.quantity
+    const weight = item.weight
+    return sum + weight * quantity
+  }, 0)
+  const dimensions = placeItems.map((item) => {
+    const height = item.height
+    const width = item.width
+    const length = item.length
+    const quantity = item.quantity
+    const sorted = [height, width, length].sort((a, b) => a - b)
+    return {
+      minSum: sorted[0] * quantity,
+      mid: sorted[1],
+      max: sorted[2],
+    }
+  })
+  const placeHeight = dimensions.reduce((s, d) => s + d.minSum, 0)
+  const placeLength = Math.max(...dimensions.map((d) => d.mid))
+  const placeWidth = Math.max(...dimensions.map((d) => d.max))
   const place = {
-    //height: placeDimensions.height,
-    //length: placeDimensions.length,
-    //width: placeDimensions.width,
+    height: placeHeight,
+    length: placeLength,
+    width: placeWidth,
     weight: totalWeight,
     items: placeItems,
+  }
+
+  const assessedCost =
+    placeItems.reduce((sum, item) => {
+      const assessedCost = item.assessedCost
+      const quantity = item.quantity
+      return sum + assessedCost * quantity
+    }, 0)
+  const deliveryCost = order.shipping_methods![0].amount as number
+  const itemsCost =
+    placeItems.reduce((sum, item) => {
+      const cost = item.cost
+      const quantity = item.quantity
+      return sum + cost * quantity
+    }, 0)
+  const codCost = isCod ? itemsCost + deliveryCost : 0
+  const cost = {
+    deliveryCostVat: -1,
+    codCost,
+    assessedCost,
+    // TODO: in example DeliveryPayedByRecipient always
+    isDeliveryPayedByRecipient: isCod ? true : false,
+    // TODO: in example there is no paymentMethod at all
+    ...(isCod ? {paymentMethod: 3} : {}),
+    ...(isCod ? {deliveryCost} : {}),
   }
 
   const apishipOrder: OrderRequest = {
@@ -105,14 +141,7 @@ export function mapToApishipOrderRequest(
       clientNumber: order.shipping_address?.customer_id || `medusa-${Date.now()}`,
       weight: totalWeight,
     },
-    cost: {
-      assessedCost: assessedCost,
-      deliveryCost: 200,
-      deliveryCostVat: -1,
-      codCost: assessedCost + 200,
-      isDeliveryPayedByRecipient: false,
-      paymentMethod: 2,
-    },
+    cost,
     sender,
     recipient,
     places: [place],
