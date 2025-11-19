691f172f08350d820bc819230908420a61c2c9c5
KaneFlak <kflak@yandex.ru>
2025-11-19 11:51:09 +0300

    refactor: implement batch import

--- packages/medusa-marketplace-wildberries/src/workflows/product/steps/import-products.ts
+++ packages/medusa-marketplace-wildberries/src/workflows/product/steps/import-products.ts
@@ -11,16 +11,6 @@ export const importProductsStep = createStep(
     const logger = container.resolve("logger")
     const query = container.resolve("query")
 
-    const nmIDs: Record<string, number> = {}
-    const imtIDs: Record<string, number> = {}
-    const sizeSkus: Record<string, Array<string>> = {}
-    let updateVariants: any[] = []
-    let updateProducts: any[] = [] 
-
-
-    logger.info("Getting data from wildberries...")
-
-    const cards: ContentV2GetCardsListPost200ResponseCardsInner[] = []
     const limit = 100
     let body = {
       "settings": {
@@ -34,66 +24,69 @@ export const importProductsStep = createStep(
     }
     let total: number
     do {
-      const { status, data: {cards: resCards, cursor} } = await productApi.contentV2GetCardsListPost(body)
-      cards.push(...resCards!)
+      const { status, data: {cards, cursor} } = await productApi.contentV2GetCardsListPost(body)
       total = cursor!.total!
       body.settings.cursor["updatedAt"] = cursor!.updatedAt
       body.settings.cursor["nmID"] = cursor!.nmID
+
+      const nmIDs: Record<string, number> = {}
+      const imtIDs: Record<string, number> = {}
+      const sizeSkus: Record<string, Array<string>> = {}
+      const skus: string[] = []
+      const updateVariants: any[] = []
+      const updateProducts: any[] = []
+
+      cards!.forEach(card => {
+        skus.push(card.vendorCode!)
+        nmIDs[card.vendorCode!] = card.nmID!
+        imtIDs[card.vendorCode!] = card.imtID!
+        sizeSkus[card.vendorCode!] = card.sizes![0].skus!
+      })
+
+      const { data: variants } = await query.graph({
+        entity: "product_variant",
+        fields: ["id", "sku", "metadata", "product.id", "product.metadata"],
+        filters: {
+          "sku": skus
+        }
+      })
+
+      variants.forEach(variant => {
+        if (variant.sku !== null && nmIDs[variant.sku]) {
+          let variantMetadata = variant.metadata ?? {}
+          variantMetadata.wildberries_nmID = nmIDs[variant.sku]
+          variantMetadata.wildberries_sizeSkus = sizeSkus[variant.sku]
+    
+          updateVariants.push({
+            id: variant.id,
+            metadata: variantMetadata
+          })
+  
+          let productMetadata = variant.product?.metadata ?? {}
+          productMetadata.wildberries_imtID = imtIDs[variant.sku]
+  
+          updateProducts.push({
+            id: variant.product?.id,
+            metadata: productMetadata
+          })
+        }      
+      })
+
+      const { result: { updated: variantsUpdateResult } } = await batchProductVariantsWorkflow(container).run({
+        input: {
+          update: updateVariants
+        }
+      })
+
+      const { result: { updated: productUpdateResult } } = await batchProductsWorkflow(container).run({
+        input: {
+          update: Array.from(
+            new Map(updateProducts.map(item => [item.id, item])).values()
+          )
+        }
+      })
     } while (total === limit)
 
-    cards.forEach(card => {
-      nmIDs[card.vendorCode!] = card.nmID!
-      imtIDs[card.vendorCode!] = card.imtID!
-      sizeSkus[card.vendorCode!] = card.sizes![0].skus!
-    })
-
-    const { data: variants } = await query.graph({
-      entity: "product_variant",
-      fields: ["id", "sku", "metadata", "product.id", "product.metadata"]
-    })
-
-    variants.forEach(variant => {
-      if (variant.sku !== null && nmIDs[variant.sku]) {
-        let variantMetadata = variant.metadata ?? {}
-        variantMetadata.wildberries_nmID = nmIDs[variant.sku]
-        variantMetadata.wildberries_sizeSkus = sizeSkus[variant.sku]
-  
-        updateVariants.push({
-          id: variant.id,
-          metadata: variantMetadata
-        })
-
-        let productMetadata = variant.product?.metadata ?? {}
-        productMetadata.wildberries_imtID = imtIDs[variant.sku]
-
-        updateProducts.push({
-          id: variant.product?.id,
-          metadata: productMetadata
-        })
-      }      
-    })
-
-    logger.info("Updating variants metadata...")
-    const { result: variantsUpdateResult } = await batchProductVariantsWorkflow(container).run({
-      input: {
-        update: updateVariants
-      }
-    })
-
-    logger.info("Updating products metadata...")
-    const { result: productUpdateResult } = await batchProductsWorkflow(container).run({
-      input: {
-        update: Array.from(
-          new Map(updateProducts.map(item => [item.id, item])).values()
-        )
-      }
-    })
-
-    const result = {
-      variantsUpdateResult: variantsUpdateResult,
-      productUpdateResult: productUpdateResult
-    }
-
-    return new StepResponse(result)
+    return new StepResponse()
   }
 )
