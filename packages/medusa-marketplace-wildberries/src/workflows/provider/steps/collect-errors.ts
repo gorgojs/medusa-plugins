36e341788cc9eb54deae7e6c58b5a93c103489ad
KaneFlak <kflak@yandex.ru>
2025-11-26 14:03:42 +0300

    feat: add error message in product variants metadata

--- packages/medusa-marketplace-wildberries/src/workflows/provider/steps/collect-errors.ts
+++ packages/medusa-marketplace-wildberries/src/workflows/provider/steps/collect-errors.ts
@@ -1,12 +1,14 @@
 import { createStep, StepResponse } from "@medusajs/framework/workflows-sdk"
 import { RequestPublicViewerPublicErrorsTableListV2 } from "../../../lib/wildberries-products-client"
 import { productApi } from "../../../lib/wildberries-client"
+import { batchProductVariantsWorkflow } from "@medusajs/medusa/core-flows"
 
 export const collectErrorsStepId = "collect-errors"
 
 export const collectErrorsStep = createStep(
   collectErrorsStepId,
   async (_, { container }) => {
+    const query = await container.resolve("query")
 
     const result: any[] = []
 
@@ -27,6 +29,35 @@ export const collectErrorsStep = createStep(
         body.cursor.batchUUID = response.data.cursor.batchUUID
       }
       next = response.data.cursor.next
+
+      const errors = response.data.items.reduce((acc, item) => {
+        Object.assign(acc, item.errors)
+        return acc
+      }, {})
+
+      const { data: variants } = await query.graph({
+        entity: "product_variant",
+        fields: ["id", "sku", "metadata"],
+        filters: {
+          "sku": Object.keys(errors)
+        }
+      })
+      
+      const updateVariants: any[] = []
+      variants.forEach(variant => {
+        let variantMetadata = variant.metadata ?? {}
+        variantMetadata.wildberries_error = errors[variant.sku]
+        updateVariants.push({
+          id: variant.id,
+          metadata: variantMetadata
+        })
+      })
+
+      const { result: { updated: variantsUpdateResult } } = await batchProductVariantsWorkflow(container).run({
+        input: {
+          update: updateVariants
+        }
+      })
     } while (next)
 
     return new StepResponse(result)
