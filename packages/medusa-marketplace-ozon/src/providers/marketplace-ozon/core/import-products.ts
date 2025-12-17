e2238ed1908654472638810d8b7f0dfeae064485
gorgohead <head@gorgojs.com>
2025-12-17 10:23:40 +0100

    fix: cleanup import-products

--- packages/medusa-marketplace-ozon/src/providers/marketplace-ozon/core/import-products.ts
+++
@@ -1,8 +0,0 @@
-import { MedusaContainer } from "@medusajs/framework"
-import { importMarketplaceProductsWorkflow } from "../../../workflows/provider"
-
-export const importProducts = async (input, container?: MedusaContainer) => {
-  const { result } = await importMarketplaceProductsWorkflow(container).run()
-
-  return result
-}
--- packages/medusa-marketplace-ozon/src/providers/marketplace-ozon/core/index.ts
+++ packages/medusa-marketplace-ozon/src/providers/marketplace-ozon/core/index.ts
@@ -1,4 +1,3 @@
 export * from "./export-products"
-export * from "./import-products"
 export * from "./get-marketplace-products-status"
 export * from "./update-marketplace-products-status"
