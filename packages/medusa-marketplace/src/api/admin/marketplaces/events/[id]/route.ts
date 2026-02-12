d44c80ac155685a56edad7f81157ff90a02a62bb
Ismailov Maxim <guarus19@gmail.com>
2026-02-12 12:13:33 +0300

    chore(medusa-marketplace): refactor to listAndCount api

--- packages/medusa-marketplace/src/api/admin/marketplaces/events/[id]/route.ts
+++ packages/medusa-marketplace/src/api/admin/marketplaces/events/[id]/route.ts
@@ -3,14 +3,29 @@ import { MarketplaceModuleService } from "../../../../../modules/marketplace/ser
 import { MARKETPLACE_MODULE } from "../../../../../modules/marketplace"
 import { AdminMarketplaceEventResponse } from "../../../../../types"
 
-export const GET = async (
-  req: MedusaRequest,
-  res: MedusaResponse<AdminMarketplaceEventResponse>
-) => {
-  const marketplaceService: MarketplaceModuleService = await req.scope.resolve(MARKETPLACE_MODULE)
-  const event = await marketplaceService.retrieveMarketplaceEvent(req.params.id, {
-    select: req.queryConfig.fields
-  })
+export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
+  const marketplaceService: MarketplaceModuleService =
+    req.scope.resolve(MARKETPLACE_MODULE)
 
-  return res.status(200).json({ event })
+  const limit = Number(req.query.limit) || 20
+  const offset = Number(req.query.offset) || 0
+
+  const [events, count] =
+    await marketplaceService.listAndCountMarketplaceEvents(
+      {
+        marketplace_id: req.query.marketplace_id as string,
+      },
+      {
+        take: limit,
+        skip: offset,
+        select: req.queryConfig?.fields,
+      }
+    )
+
+  return res.status(200).json({
+    events,
+    count,
+    limit,
+    offset,
+  })
 }
