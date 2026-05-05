03823641bbd4df9a0934b453ac8918752ffb5601
ca11ba <amirgizatullin176@gmail.com>
2026-05-05 11:09:48 +0300

    refactor(vite-plugin): refactor to integration

--- packages/utils/medusa-vite-plugin/src/widgets/integration/utils.ts
+++ packages/utils/medusa-vite-plugin/src/widgets/integration/utils.ts
@@ -1,9 +1,9 @@
 import { INJECTION_ZONES } from "./constants"
-import { MarketplaceInjectionZone } from "./types"
+import { IntegrationInjectionZone } from "./types"
 
 /**
  * Validates that the provided zone is a valid injection zone for a widget.
  */
-export function isValidInjectionZone(zone: any): zone is MarketplaceInjectionZone {
+export function isValidInjectionZone(zone: any): zone is IntegrationInjectionZone {
   return INJECTION_ZONES.includes(zone)
 }
--- packages/utils/medusa-vite-plugin/src/widgets/marketplace/types.ts
+++
@@ -1,3 +0,0 @@
-import { INJECTION_ZONES } from "./constants"
-
-export type MarketplaceInjectionZone = (typeof INJECTION_ZONES)[number]
