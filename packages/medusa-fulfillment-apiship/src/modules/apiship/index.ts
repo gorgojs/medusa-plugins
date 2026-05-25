fafd0efa63b2ec65a6657ae63711bf18e7cc34eb
ttokyose <parsifal.cs@gmail.com>
2026-05-26 00:17:48 +0300

    fix(fulfillment-apiship): remove apiship model and options

--- packages/medusa-fulfillment-apiship/src/modules/apiship/index.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from "@medusajs/framework/utils"
-import ApishipSettingsService from "./services/apiship-settings-service"
-
-export const APISHIP_MODULE = "apiship"
-
-export default Module(APISHIP_MODULE, {
-  service: ApishipSettingsService,
-})
--- packages/medusa-fulfillment-apiship/src/modules/apiship/models/apiship-settings.ts
+++
@@ -1,9 +0,0 @@
-import { model } from "@medusajs/framework/utils"
-
-export const ApishipSettings = model.define("apiship_setting", {
-  id: model.id().primaryKey(),
-  is_test: model.boolean(),
-  is_enabled: model.boolean(),
-  token: model.text(),
-  enable_logs: model.boolean(),
-})
