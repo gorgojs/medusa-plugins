6276be36e3b61ad6ee1cc29fd44735e5209752d0
gorgohead <head@gorgojs.com>
2026-07-24 14:58:50 +0300

    chore: fix monorepo

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,17 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 
-const PATHS_TO_DIR = ['packages', 'packages/utils'];
+const PATHS_TO_DIR = ['packages/modules/', 'packages/providers/', 'packages/utils'];
 const REPO_SCOPES = ['deps', 'release', 'docs', 'root'];
 
 const packageScopes = PATHS_TO_DIR.flatMap(dirPath => {
   const fullPath = path.resolve(__dirname, dirPath);
   if (!fs.existsSync(fullPath)) return [];
-  const stripMedusa = dirPath === 'packages';
   return fs.readdirSync(fullPath, { withFileTypes: true })
     .filter(d => d.isDirectory())
     .filter(d => fs.existsSync(path.join(fullPath, d.name, 'package.json')))
-    .map(d => stripMedusa ? d.name.replace(/^medusa-/, '') : d.name);
+    .map(d => d.name);
 });
 
 module.exports = {
